1921b02599881a9a11633b2dc8a86b63e9ba3d47
Tanzila-Khan <70341551+Tanzila-Khan@users.noreply.github.com>
2021-10-01 01:04:54 +0530

    Delete internship directory

--- internship/src/main/java/com/inspire/internship/service/ExcelDataService.java
+++
@@ -1,9 +0,0 @@
-package com.inspire.internship.service;
-import java.util.List;
-
-import com.inspire.internship.entity.*;
-public interface ExcelDataService {
-	List<inspire> getExcelDataAsList(String file);
-	
-	int saveExcelData(List<inspire> invoices);
-}
--- internship/src/main/java/com/inspire/internship/service/FileUploaderService.java
+++
@@ -1,7 +0,0 @@
-package com.inspire.internship.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileUploaderService {
-	public void uploadFile(MultipartFile file);
-}
